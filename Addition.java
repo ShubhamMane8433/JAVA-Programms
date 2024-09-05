8955b9987f3e868efa12a08ffd1415ab21ba71a9
ShubhamMane8433 <shubhammane9822@gmail.com>
2024-09-06 00:07:35 +0530

    changes

--- Addition.java
+++ Addition.java
@@ -2,18 +2,18 @@ class Addition
 {
 	public static void sum(int number1, int number2)
 	{
-		System.out.println("sum method started!!!");
+		System.out.println("sum method started...!!!");
 		int result = 0;
 		result = number1 + number2;
 		System.out.println(result);
-		System.out.println("sum method ended!!!");
+		System.out.println("sum method ended...!!!");
 	}
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Main method started!!!");
+		System.out.println("Main method started...!!!");
 		sum(40, 30);
-		System.out.println("Main method ended!!!");
+		System.out.println("Main method ended...!!!");
 	}
 	
 
